9fdef9d17d48f5ac7223f076abcca063e707359a
notprestn <163453987+notprestn@users.noreply.github.com>
2024-03-19 14:09:31 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'token': 'MTIxOTYxMjk1MDA3NjA2Mzc0Ng.GoYMAo.JxTSMvQWV1aYNGV46G3FrbllWjcG1JCKbEjYQ0', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1219612950076063746', // Your discord bot id.
+    'guildid': '1219612781234360380', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['1213902568267448350'], // Add discord ids to the array to access the commands.
+    'webhook': 'https://discord.com/api/webhooks/1219612850473926696/rqK3vkmIMvbFHc7ZcMmbzEaQjSnfjxFz6f4K4JM6s26YJrjMX0UTCWt0pyev-kW3xFKo' // Your discord webhook url.
+}
